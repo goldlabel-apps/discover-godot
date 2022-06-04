4ac7652b991136de667d217fc4cfe854b3e6559e
milky <listingslab@gmail.com>
2022-06-04 11:15:34 +0200

    Start routing req to get a sensible bit of JSON

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,18 +1,44 @@
 import * as functions from "firebase-functions";
 
 export const serverSideRender = functions.https.onRequest(
-    (request, response) => {
-      functions.logger.info("serverSideRender", {
+    (req, response) => {
+      const pathname = req.params[0];
+      functions.logger.info("SSR", {
         structuredData: true,
+        pathname,
       });
-      response.send("serverSideRender");
+      response.send("<html>SSR<html>");
     });
 
-
-export const proxyAPI = functions.https.onRequest(
-    (request, response) => {
-      functions.logger.info("serverSiproxyAPIdeRender", {
-        structuredData: true,
-      });
-      response.send("proxyAPI");
-    });
+export const proxyAPI = functions.https.onRequest( async (req, response) => {
+  
+  const baseAPIUrl = getBaseAPIUrl( req );
+  let params = req.params[0].split( `/` )
+	params = params.slice( 1, params.length)
+	let endpoint = params[0]
+  const { method } = req
+  const json = {
+    response:{
+      id: "0001",
+      data:{
+        title:"Title for Hello World game",
+        subheader:"Hello World subheader",
+      }
+    },
+    req:{
+      time: Date.now(),
+      baseAPIUrl,
+      endpoint,
+      method,
+    },
+  };
+  response.send(json);
+});
+    
+function getBaseAPIUrl (req:any) {
+	let baseAPIUrl = `https://discover-godot.web.app/`
+	if ( req.hostname === 'localhost' ){
+		baseAPIUrl = `http://localhost:5001/discover-godot/us-central1/proxyAPI/`
+	}
+	return baseAPIUrl
+}
